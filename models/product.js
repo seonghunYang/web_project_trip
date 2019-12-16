9069b96286a64b7e3a957c5f09e5b3c42aa96424
unknown <didtjdgns852@gmail.com>
2019-12-16 21:06:21 +0900

    fix

--- models/product.js
+++ models/product.js
@@ -10,6 +10,7 @@ var schema = new Schema({
   img: {type: String},
   detail_content: {type: String}, //wysiwyg에티터로 받는거
   price: {type: Number}, // 인원에 따라 변하는 가격 생각
+  numComments: {type: Number, default: 0},
   numReads: {type: Number, default: 0},
   numLikes: {type: Number, default: 0},
   createdAt: {type: Date, default: Date.now},
